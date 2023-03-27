fa5979e9eaa213e5ed8fcc05ee98631462b05d77
Sidharth Bhasin <sid21bhasin@gmail.com>
2023-03-28 00:30:49 +0530

    edit card also added

--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -1,30 +1,114 @@
+import { useState, useEffect, useRef } from "react";
 import VideoCard from "./VideoCard";
-import { useSelector } from "react-redux";
-
+import { useSelector, useDispatch } from "react-redux";
+import { v4 as uuid } from "uuid";
+import {
+  moveCardToCategory,
+  removeCardFromCategory,
+  setNewCategory,
+} from "../features/reducers/cardSlice";
 const VideoContainer = () => {
-  const { categories } = useSelector((state) => state.cardsReducer);
+  const ref = useRef();
+  const dispatch = useDispatch();
+  const [tempCategoryName, setTempCategoryName] = useState("");
+  const [activeCardData, setActiveCardData] = useState(null);
+  const [activeCategory, setActiveCategory] = useState(null);
+  const [items, setItems] = useState([]);
+  const c = useSelector((state) => state.cardsReducer);
+  const { categories } = c;
   const categoryNames = Object.keys(categories);
 
-  const handleActiveCategory = () => {};
+  const moveCard = (moveTo, a, _cat) => {
+    console.log(a, _cat);
+    console.log(ref);
+    console.log(activeCategory);
+    // return;
+    // return;
+    dispatch(
+      removeCardFromCategory({
+        name: _cat?.name,
+        cardId: ref.current?.id,
+      })
+    );
+    dispatch(
+      moveCardToCategory({
+        name: moveTo,
+        data: ref.current,
+      })
+    );
+  };
+
+  const handleActiveCategory = (data) => {
+    setActiveCategory(categories[data]);
+  };
+
+  const handleChange = (e) => {
+    setTempCategoryName(e.target.value);
+  };
+
+  const addCategory = (e) => {
+    e.preventDefault();
+    dispatch(setNewCategory(tempCategoryName));
+    setTempCategoryName("");
+  };
+
+  useEffect(() => {
+    setActiveCategory(categories[activeCategory?.name]);
+  }, [categories]);
+
+  useEffect(() => {
+    if (activeCategory) {
+      let _items = categoryNames.filter((_data) => _data);
+      let localArr = _items.map((_data) => {
+        return {
+          key: uuid(),
+          label: (
+            <button
+              onClick={() => moveCard(_data, activeCardData, activeCategory)}
+            >
+              {_data}
+            </button>
+          ),
+        };
+      });
+      setItems(localArr);
+    }
+  }, [activeCategory]);
 
   return (
-    <>
+    <div>
       <ul>
-        {categoryNames.map((data, i) => {
+        {categoryNames?.map((data, i) => {
           return (
-            <li key={i} onClick={handleActiveCategory}>
+            <li key={i} onClick={() => handleActiveCategory(data)}>
               {data}
             </li>
           );
         })}
       </ul>
-      <div className="video-container">
-        <VideoCard />
-        <VideoCard />
-        <VideoCard />
-        <VideoCard />
+      <div>
+        <form onSubmit={addCategory}>
+          <input onChange={handleChange} value={tempCategoryName} />
+        </form>
+        <button>History</button>
       </div>
-    </>
+      <div className="video-container">
+        {activeCategory
+          ? activeCategory.data.map((_data) => {
+              return (
+                <VideoCard
+                  data={_data}
+                  name={_data.name}
+                  key={_data.id}
+                  category={activeCategory}
+                  items={items}
+                  setActiveCardData={ref}
+                />
+              );
+            })
+          : ""}
+      </div>
+    </div>
   );
 };
 
