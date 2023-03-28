8332c1ea32d67d0dd3557940e31007ebc6de27fe
Sidharth Bhasin <sid21bhasin@gmail.com>
2023-03-28 09:27:21 +0530

    history functionality added

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -1,11 +1,165 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button, Dropdown, Modal } from "antd";
+import { EllipsisOutlined } from "@ant-design/icons";
+import { useDispatch } from "react-redux";
+import YouTube from "react-youtube";
+import {
+  addToHistory,
+  removeCardFromCategory,
+  renameCard,
+} from "../features/reducers/cardSlice";
+import Input from "antd/es/input";
+const VideoCard = ({ data, name, category, items, setActiveCardData }) => {
+  //   console.log(data);
 
-const VideoCard = () => {
+  const dispatch = useDispatch();
+  const [isModalOpen, setIsModalOpen] = useState({
+    viewer: false,
+    editor: false,
+  });
+  const [inputData, setInputData] = useState({
+    name: data.name,
+    link: data.link,
+  });
+
+  const onPlayerReady = (event) => {
+    // access to player in all event handlers via event.target
+    event.target.pauseVideo();
+  };
+
+  const opts = {
+    height: "390",
+    width: "640",
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+  };
+
+  const handleMove = () => {
+    setActiveCardData.current = data;
+    console.log("hereeee");
+  };
+
+  const showModal = (type) => {
+    if (type === "editor") {
+      setIsModalOpen({
+        ...isModalOpen,
+        editor: true,
+      });
+    } else {
+      setIsModalOpen({
+        ...isModalOpen,
+        viewer: true,
+      });
+
+      dispatch(
+        addToHistory({
+          date: new Date(),
+          name: data.name,
+          link: data.link,
+        })
+      );
+    }
+  };
+
+  const handleOk = () => {
+    setIsModalOpen({
+      editor: false,
+      viewer: false,
+    });
+    dispatch(
+      renameCard({
+        name: category.name,
+        link: inputData.link,
+        cardId: data.id,
+        cardName: inputData.name,
+      })
+    );
+    setInputData({
+      name: data.name,
+      link: data.link,
+    });
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen({
+      viewer: false,
+      editor: false,
+    });
+    setInputData({
+      name: data.name,
+      link: data.link,
+    });
+  };
+
+  const handleCardInfoChange = (e) => {
+    setInputData({
+      ...inputData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  console.log(inputData);
   return (
-    <div className="video-card">
-      <iframe src="https://www.youtube.com/watch?v=wU1cQcQFimw" />
-      <span>Name</span> <span>category</span>
-    </div>
+    <>
+      <div className="video-card">
+        <span>{name}</span> <span>{category.name}</span>
+        <Button onClick={() => showModal("viewer")}>View</Button>
+        <Button onClick={() => showModal("editor")}>Edit</Button>
+        <Dropdown
+          menu={{ items }}
+          trigger="click"
+          placement="bottomLeft"
+          arrow={{ pointAtCenter: true }}
+        >
+          <Button onClick={() => handleMove()}>Move</Button>
+        </Dropdown>
+        <Button
+          onClick={() => {
+            console.log(name, data);
+
+            //   return;
+            dispatch(
+              removeCardFromCategory({
+                name: category.name,
+                cardId: data.id,
+              })
+            );
+          }}
+        >
+          Delete
+        </Button>
+      </div>
+      <Modal
+        title="Basic Modal"
+        open={isModalOpen.editor}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        <Input
+          value={inputData.name}
+          placeholder="video name"
+          name="name"
+          onChange={handleCardInfoChange}
+        />
+        <Input
+          value={inputData.link}
+          placeholder="video link"
+          name="link"
+          onChange={handleCardInfoChange}
+        />
+      </Modal>
+      <Modal
+        width={700}
+        title="Video Viewer"
+        open={isModalOpen.viewer}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        <YouTube videoId="2g811Eo7K8U" opts={opts} onReady={onPlayerReady} />
+      </Modal>
+    </>
   );
 };
 
--- src/features/reducers/cardSlice.js
+++ src/features/reducers/cardSlice.js
@@ -7,17 +7,17 @@ const initialState = {
       name: "education",
       data: [
         {
-          id: 1,
+          id: uuid(),
           name: "1 some video",
           link: "https: //shubambhasin.com",
         },
         {
-          id: 2,
+          id: uuid(),
           name: "2 some video",
           link: "https: //shubambhasin.com",
         },
         {
-          id: 3,
+          id: uuid(),
           name: "3 some video",
           link: "https://shubambhasin.com",
         },
@@ -28,23 +28,24 @@ const initialState = {
       name: "motivation",
       data: [
         {
-          id: 1,
+          id: uuid(),
           name: "1 motivation Video",
           link: "https: //shubambhasin.com",
         },
         {
-          id: 2,
+          id: uuid(),
           name: "2  video",
           link: "https: //shubambhasin.com",
         },
         {
-          id: 3,
+          id: uuid(),
           name: "3 some video",
           link: "https://shubambhasin.com",
         },
       ],
     },
   },
+  history: [],
 };
 
 const cardSlice = createSlice({
@@ -57,15 +58,109 @@ const cardSlice = createSlice({
         categories: {
           ...state.categories,
           [action.payload]: {
-            id: 1,
+            id: uuid(),
             name: action.payload,
             data: [],
           },
         },
       };
     },
+    moveCardToCategory: (state, action) => {
+      console.log(action);
+      console.log(state.categories[action.payload.name]);
+      // return;
+      return {
+        ...state,
+        categories: {
+          ...state.categories,
+          [action.payload.name]: {
+            ...state.categories[action.payload.name],
+            data: [
+              ...state.categories[action.payload.name].data,
+              {
+                id: uuid(),
+                name: action.payload.data?.name,
+                link: action.payload.data?.link,
+              },
+            ],
+          },
+        },
+      };
+    },
+    removeCardFromCategory: (state, action) => {
+      console.log(action);
+
+      // return;
+      return {
+        ...state,
+        categories: {
+          ...state.categories,
+          [action.payload.name]: {
+            ...state.categories[action.payload.name],
+            data: state.categories[action.payload.name]?.data.filter(
+              (_data) => _data.id !== action.payload.cardId
+            ),
+          },
+        },
+      };
+    },
+    renameCard: (state, action) => {
+      console.log(action);
+      return {
+        ...state,
+        categories: {
+          ...state.categories,
+          [action.payload.name]: {
+            ...state.categories[action.payload.name],
+            data: state.categories[action.payload.name]?.data.map((_data) => {
+              if (_data.id === action.payload.cardId) {
+                return {
+                  name: action.payload.cardName,
+                  link: action.payload.link,
+                };
+              } else {
+                return {
+                  ..._data,
+                };
+              }
+            }),
+          },
+        },
+      };
+    },
+    addToHistory: (state, action) => {
+      console.log(action);
+      return {
+        ...state,
+        history: [
+          ...state.history,
+          {
+            ...action.payload,
+          },
+        ],
+      };
+    },
   },
 });
 
-export const { setNewCategory } = cardSlice.actions;
+export const moveCard = (data, categoryName) => {
+  moveCardToCategory({
+    name: categoryName,
+    data: data,
+  });
+};
+export const removeCard = (categoryName, cardId) => {
+  removeCardFromCategory({
+    name: categoryName,
+    cardId: cardId,
+  });
+};
+
+export const {
+  setNewCategory,
+  moveCardToCategory,
+  removeCardFromCategory,
+  renameCard,
+  addToHistory,
+} = cardSlice.actions;
 export default cardSlice.reducer;
